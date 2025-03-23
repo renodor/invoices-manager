cc91708546376c9b6b0fb3955f6ab4252cf9f4b9
renodor <renaud.dor@gmail.com>
2025-03-23 19:31:01 +0100

    Use DescriptionBlock model

--- app/controllers/description_blocks_controller.rb
+++ app/controllers/description_blocks_controller.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 class DescriptionBlocksController < ApplicationController
+  before_action :set_quote
+
   def new
-    @quote = current_user.quotes.find(params[:quote_id])
     @description_block = @quote.description_blocks.new(flavor: params[:flavor])
   end
 
   def create
-    @quote = current_user.quotes.find(params[:quote_id])
     @description_block = @quote.description_blocks.build(description_block_params)
     @description_block.position = (@quote.description_blocks.ordered.last&.position || 0) + 1
-    @description_block.text = @description_block.text.gsub('- ', '') if @description_block.flavor == 'list'
 
     if @description_block.save
       respond_to do |format|
@@ -22,7 +21,37 @@ class DescriptionBlocksController < ApplicationController
     end
   end
 
+  def edit
+    @description_block = @quote.description_blocks.find(params[:id])
+  end
+
+  def update
+    @description_block = @quote.description_blocks.find(params[:id])
+    @description_block.text = description_block_params[:text]
+    if @description_block.save
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: 'Description block was successfully updated.' }
+        format.turbo_stream { flash.now[:notice] = 'Description block was successfully updated.' }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @description_block = @quote.description_blocks.find(params[:id]).destroy
+
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: 'Description block was successfully destroyed.' }
+      format.turbo_stream { flash.now[:notice] = 'Description block was successfully destroyed.' }
+    end
+  end
+
   private
+
+  def set_quote
+    @quote = current_user.quotes.find(params[:quote_id])
+  end
 
   def description_block_params
     params.require(:description_block).permit(:text, :flavor)
