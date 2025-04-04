be4fec8081c5ee082359c8b90824dda189660adb
Renaud Dor <renaud.dor@gmail.com>
2025-04-04 17:31:40 +0200

    Add up route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal:true
 
 Rails.application.routes.draw do
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Needed for kamal setup
+  get "up" => "rails/health#show", as: :rails_health_check
+
   root to: "invoices#index"
 
   devise_for :users
